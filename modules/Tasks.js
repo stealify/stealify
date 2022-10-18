98133761388f99e6378f2fc84b2888a8c62e7936
Frank Lemanschik <frank@lemanschik.com>
2022-10-18 11:32:47 +0200

    Create Tasks.js
    Signed-off-by: Frank Lemanschik <frank@lemanschik.com>

---
+++ modules/Tasks.js
@@ -0,0 +1,4 @@
+/**
+ * A Module that exposes Generic Tasks that get often used and Combined to create high level Tasks like:
+ * OpenFile, OpenFileDescriptor, CreateWebRTCSocket, .... CreateHttpServer 
+ */

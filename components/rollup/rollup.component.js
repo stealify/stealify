669cb033608aed7cd40b6921d54ab09667ef659d
Frank Lemanschik <frank@lemanschik.com>
2022-10-19 08:51:05 +0200

    Create rollup.component.js
    Signed-off-by: Frank Lemanschik <frank@lemanschik.com>

---
+++ components/rollup/rollup.component.js
@@ -0,0 +1,7 @@
+/**
+ * Rollup Component definition
+ * implements component-rollup use able via 
+ * import { rollup } from 'component:rollup';
+ * const { rollup } = await import('component:rollup');
+ * rollup(Task, Task, Task, Task)
+ */

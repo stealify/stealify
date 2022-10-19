2ed96f6ce4a5a00ad21c77c780a002cf4c82f0a2
Frank Lemanschik <frank@lemanschik.com>
2022-10-19 08:54:21 +0200

    Update node.component.js
    Signed-off-by: Frank Lemanschik <frank@lemanschik.com>

--- components/node/node.component.js
+++ components/node/node.component.js
@@ -1,3 +1,6 @@
 /**
  * A NodeJS Component
+ * offers also component:node-compat which offers a 
+ * compatability interface for all NodeJS versions as also .node modules
+ * first implementation will simply try to get NodeJS Modules mapped to stealifys component-just.
  */
